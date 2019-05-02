2cc154f929aa35011f6a8eefd70bdc8fcf247d7b
Jxic <jxjim98@live.com>
2019-05-02 11:57:33 +0100

    convenient include of utils.h from C++

--- C/utils.h
+++ C/utils.h
@@ -1,12 +1,20 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "matrix_op.h"
 
 #define BUFFER_SIZE 256
 
 double rand_uniform(double low, double high);
 matrix_t* load_data(char* filename);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
 
